b79b9ade523d3a8a53c86860cfb3026f77226fb1
VishwajeethV <spacexblue80@gmail.com>
2021-08-24 18:42:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ function preload() {
 }
 
 function setup() {
-	canvas = createCanvas(1240,336);
+	canvas = createCanvas(1240,336);	
+	canvas.parent("canvas");
 	instializeInSetup(mario);
+	video=CreateCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game_screen");
 }
+
 
 function draw() {
 	game()
